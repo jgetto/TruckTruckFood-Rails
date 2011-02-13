2c8c7ad8555c85032dd1bf65dd13777a53df7127
Jacob Getto <jacob.getto@gmail.com>
2011-02-13 16:13:55 -0500

    some stuff

---
+++ trucktruckfood/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucktruckfood::Application.config.secret_token = '0b81156088e975b56682a1b188a4db93f84d2d8630e7ed17ec8909a49d805511f8d8f5676b10770df89b85f7575af17961bc88a4d0ce93bb0f9099569f9b74db'
