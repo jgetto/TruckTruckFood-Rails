9337a14b939cb5f4cd2410acab57a0ca588505d8
asetjoadi <albert.jsetjoadi@gmail.com>
2011-02-13 18:26:15 -0500

    Cartographer plugin added,but not working yet

--- app/controllers/trucks_controller.rb
+++ app/controllers/trucks_controller.rb
@@ -1,5 +1,17 @@
 class TrucksController < ApplicationController
   def map
+    @map = Cartographer::Gmap.new( 'map' )
+    @map.zoom = :bound
+    @map.icons << Cartographer::Gicon.new
+    marker1 = Cartographer::Gmarker.new(:name=> "taj_mahal", :marker_type => "Building",
+                :position => [27.173006,78.042086],
+                :info_window_url => "/url_for_info_content")
+    marker2 = Cartographer::Gmarker.new(:name=> "raj_bhawan", :marker_type => "Building",
+                :position => [28.614309,77.201353],
+                :info_window_url => "/url_for_info_content")
+
+    @map.markers << marker1
+    @map.markers << marker2
   end
 
   def locate
