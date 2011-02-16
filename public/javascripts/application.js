d21b7196d20aba1fa23c5790d63f781a996c6d44
Jacob Getto <jacob.getto@gmail.com>
2011-02-16 01:34:18 -0500

    jquery and columns

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+                //hide the all of the element with class msg_body
+                $(".expandy").hide();
+                //toggle the componenet with class msg_body
+                $(".row").click(function() {
+                    $(".expandy").slideUp(600);
+                    if (!$(this).children().next(".expandy").is(":visible")){
+                        $(this).children().next(".expandy").slideToggle(600);
+                    }
+                });
+});
